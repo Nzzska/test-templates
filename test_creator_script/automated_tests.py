486f4da260620212eb3acb87bbd2fb37ca25c5c6
Oskaras <66420051+Nzzska@users.noreply.github.com>
2022-04-25 02:05:29 +0300

    vakarinis

--- test_creator_script/automated_tests.py
+++ test_creator_script/automated_tests.py
@@ -1,4 +1,5 @@
 from main_v2 import Test
+from photoelectric_effect_problems import Table_problem
 
 #Testing Test.get_questions()
 def test_get_questions():
@@ -57,4 +58,31 @@ trial_test.form_multicolumns(2)
 trial_test.replace_placeholders("%Q0%")
 trial_test.get_questions('./insert/', 4)
 trial_test.replace_placeholders("%Q1%")
+
+trial_problem = Table_problem('islaisvinimo_darbai.json')
+data = trial_problem.gather_data("metalai")
+trial_table = trial_problem.form_tabular(' |c|c|c| ', data)
+
+data_two = trial_problem.gather_data("fotonai")
+trial_table_two = trial_problem.form_tabular(' |c|c|c| ', data_two)
+
+trial_table += trial_table_two
+
+trial_test.content = trial_table
+trial_test.form_multicolumns(2)
+trial_test.replace_placeholders("%Q2%")
+trial_placeholders = {
+    '0': ['Li', 'Na', 'Cs', 'Ca', 'Nb', 'Zr'],
+    '1': ['violetinės', 'mėlynos', 'žydros', 'žalios']
+}
+trial_problem.formulate_problem(
+    'trecia_užduotis.tex',
+    trial_placeholders,
+    'trecia',
+    12,
+    'trecia'
+)
+trial_test.get_questions('./trecia/', 1)
+trial_test.replace_placeholders("%Q3%")
+
 trial_test.export_test('./trial/trial_test.tex')
--- test_creator_script/photoelectric_effect_problems.py
+++ test_creator_script/photoelectric_effect_problems.py
@@ -1,5 +1,6 @@
 import json
 import random
+import os
 
 ev_to_J = 1.6 * 10 ** (-19)
 
@@ -8,14 +9,14 @@ class Table_problem():
         self.data_dir = data_dir
 
     def gather_data(self, variable):
-        data = json.load(open(self.data_dir))
+        data = json.load(open(self.data_dir, encoding='utf8'))
         data = data[variable]
         columns = self.init_columns(data, variable)
         for table_item in data:
             for column in data[table_item]:
                 columns[variable].append(table_item)
                 columns[column].append(data[table_item][column])
-        print(columns)
+        return columns
     
     def init_columns(self, data, variable):
         cols = {}
@@ -25,14 +26,82 @@ class Table_problem():
                 cols[column] = []
         return cols
 
-    def form_tabular(self, table_type, cols_no, rows_no):
-        start = \
+    def form_tabular(
+        self, 
+        table_type, 
+        content):
+        tabular = \
         """\\begin{{center}}
-        \\begin{{tabular}}{s} 
-        \\hline""".format(s=table_type)
-        pass
+        \\begin{{tabular}}{{{s}}} 
+        \\hline \n""".format(s=table_type)
+        cols_names = []
+        for content_name in content:
+            cols_names.append(content_name)
+        row = self.empty_table_row(len(content))
+        tabular += self.fill_row_content(
+                cols_names,
+                row
+            )
+        for i in range(0, len(content[cols_names[1]])):
+            cols = []
+            for j in content:
+                cols.append(content[j][i])
+            tabular += self.fill_row_content(
+                cols,
+                row
+            )
+        tabular += ' \n \\end{tabular} \n \\end{center} \n'
+        return tabular
+
+    def empty_table_row(self, number_of_cols):
+        output = ''
+        for i in range(0, number_of_cols):
+            if (i != number_of_cols-1):
+                output += "{"+str(i)+"} & "
+            else:
+                output += "{"+str(i)+"} \\\\ \n \\hline "
+        return output
+
+    def fill_row_content(self, content, input):
+        output = input
+        output = str(output).format(*content)
+        return output
+
+    def extract_content_row(self, row_number, content):
+        output = []
+        for column in content:
+            output.append(content[column][row_number])
+        return output
+
+    def formulate_problem(
+        self, 
+        problem_path, 
+        placeholder_replacements,
+        output_foler,
+        number_of_problems,
+        problem_naming):
+        self.create_problem_dir(output_foler)
+        problem = open(problem_path, 'r', encoding='utf8').read()
+        for j in range(0, number_of_problems):
+            replacements = []
+            for i in range(0, len(placeholder_replacements)):
+                replacements.append(
+                    random.choice(placeholder_replacements[str(i)])
+                )
+            current_problem = problem.format(*replacements)
+            current_name = problem_naming + '_' + str(j) + '.tex'
+            open(output_foler+'/'+current_name, 'w+', encoding='utf8').write(
+                current_problem
+            )
 
 
-
-trial_problem = Table_problem('islaisvinimo_darbai.json')
-trial_problem.gather_data("metalai")
+    def create_problem_dir(self, output_folder):
+        files = os.listdir()
+        if (output_folder not in files):
+            print(os.listdir())
+            os.mkdir(output_folder)
+        else:
+            pass
+# trial_problem = Table_problem('islaisvinimo_darbai.json')
+# data = trial_problem.gather_data("metalai")
+# trial_problem.form_tabular(' |c|c|c| ', data)
